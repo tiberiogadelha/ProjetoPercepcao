649fd28ec4e3fc62ea65cd2047912a874f99bae6
tiberiogadelha <tiberio.gomes@ccc.ufcg.edu.br>
2019-12-06 10:05:53 -0300

    Update test.py

--- data/test.py
+++ data/test.py
@@ -13,7 +13,7 @@ from object_detection.utils import label_map_util
 from object_detection.utils import visualization_utils as vis_util
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
-# Model preparation 
+
 PATH_TO_FROZEN_GRAPH = 'inference_graph/frozen_inference_graph.pb'
 PATH_TO_LABELS = 'data/labelmap.pbtxt'
 NUM_CLASSES = 37
@@ -35,21 +35,17 @@ with detection_graph.as_default():
 def Captcha_detection(image, average_distance_error=3):
     with detection_graph.as_default():
         with tf.Session(graph=detection_graph) as sess:
-            # Open image
             image_np = cv2.imread(image)
-            # Resize image if needed
             image_np = cv2.resize(image_np, (0,0), fx=3, fy=3) 
-            # To get real color we do this:
             image_np = cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)
-            # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
             image_np_expanded = np.expand_dims(image_np, axis=0)
-            # Actual detection.
+     
             image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
             boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
             scores = detection_graph.get_tensor_by_name('detection_scores:0')
             classes = detection_graph.get_tensor_by_name('detection_classes:0')
             num_detections = detection_graph.get_tensor_by_name('num_detections:0')
-            # Visualization of the results of a detection.
+        
             (boxes, scores, classes, num_detections) = sess.run(
               [boxes, scores, classes, num_detections],
               feed_dict={image_tensor: image_np_expanded})
@@ -61,25 +57,18 @@ def Captcha_detection(image, average_distance_error=3):
               category_index,
               use_normalized_coordinates=True,
               line_thickness=2)
-            # Show image with detection
-            #cv2.imshow(title, cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB))
-            # Save image with detection
             cv2.imwrite("captcha_result.png", cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB))
 
 
-            # Bellow we do filtering stuff
             captcha_array = []
-            # loop our all detection boxes
+            
             for i,b in enumerate(boxes[0]):
                 for Symbol in range(37):
-                    if classes[0][i] == Symbol: # check if detected class equal to our symbols
-                        if scores[0][i] >= 0.65: # do something only if detected score more han 0.65
-                                            # x-left        # x-right
+                    if classes[0][i] == Symbol: 
+                        if scores[0][i] >= 0.55:  
                             mid_x = (boxes[0][i][1]+boxes[0][i][3])/2 # find x coordinates center of letter
-                            # to captcha_array array save detected Symbol, middle X coordinates and detection percentage
-                            captcha_array.append([category_index[Symbol].get('name'), mid_x, scores[0][i]])
-
-            # rearange array acording to X coordinates datected
+                            captcha_array.append([category_index[Symbol].get('name'), mid_x, scores[0][i]]
+     
             for number in range(20):
                 for captcha_number in range(len(captcha_array)-1):
                     if captcha_array[captcha_number][1] > captcha_array[captcha_number+1][1]:
@@ -87,30 +76,24 @@ def Captcha_detection(image, average_distance_error=3):
                         captcha_array[captcha_number] = captcha_array[captcha_number+1]
                         captcha_array[captcha_number+1] = temporary_captcha
 
-
-            # Find average distance between detected symbols
             average = 0
             captcha_len = len(captcha_array)-1
             while captcha_len > 0:
                 average += captcha_array[captcha_len][1]- captcha_array[captcha_len-1][1]
                 captcha_len -= 1
-            # Increase average distance error
             average = average/(len(captcha_array)+average_distance_error)
 
             
             captcha_array_filtered = list(captcha_array)
             captcha_len = len(captcha_array)-1
             while captcha_len > 0:
-                # if average distance is larger than error distance
                 if captcha_array[captcha_len][1]- captcha_array[captcha_len-1][1] < average:
-                    # check which symbol has higher detection percentage
                     if captcha_array[captcha_len][2] >= captcha_array[captcha_len-1][2]:
                         del captcha_array_filtered[captcha_len-1]
                     else:
                         del captcha_array_filtered[captcha_len]
                 captcha_len -= 1
-
-            # Get final string from filtered CAPTCHA array
+                                                 
             captcha_string = ""
             for captcha_letter in range(len(captcha_array_filtered)):
                 captcha_string += captcha_array_filtered[captcha_letter][0]
